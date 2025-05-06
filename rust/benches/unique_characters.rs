3bdbe4c3701fe1ecad4a3c4873bd67ec64fc9b45
Will Beason <willbeason@gmail.com>
2025-05-06 10:18:58 -0500

    Add more benchmark test cases
    Signed-off-by: Will Beason <willbeason@gmail.com>

--- rust/benches/unique_characters.rs
+++ rust/benches/unique_characters.rs
@@ -1,11 +1,17 @@
-use chapter1::strings::unique_characters::unique_characters_1;
+use chapter1::strings::unique_characters::{unique_characters_1, unique_characters_2, unique_characters_3};
 use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
 
 fn bench_unique_characters(c: &mut Criterion) {
     let mut group = c.benchmark_group("unique_characters");
-    for i in ["abc"].iter() {
+    for i in ["a", "ab", "abcde", "abcdefghij", "abcdefghijklmnopqrstuv"].iter() {
         group.bench_with_input(BenchmarkId::new("unique_characters_1", i), i, |b, i| {
             b.iter(|| unique_characters_1(i))
+        });
+        group.bench_with_input(BenchmarkId::new("unique_characters_2", i), i, |b, i| {
+            b.iter(|| unique_characters_2(i))
+        });
+        group.bench_with_input(BenchmarkId::new("unique_characters_3", i), i, |b, i| {
+            b.iter(|| unique_characters_3(i))
         });
     }
     group.finish()
